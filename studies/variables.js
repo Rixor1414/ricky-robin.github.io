7dac48169f27fb28d3838091f8dac28f58df68d4
Ricky Robin <softreset13@gmail.com>
2023-08-22 00:48:17 +0000

    describe commit

---
+++ studies/variables.js
@@ -0,0 +1,23 @@
+/**
+ * VARIABLES:
+ * 
+ * 0 Varieables: are named tags for values in javascript. Variables are created using declaring keywords such as var or let.
+ * 
+ * 1 Declaration and assignment: To create a variable in JS you have to declare it using either Var, Let, or Const
+ * 
+ * 2 Var Let and Const: Var Let and Const are keywords used to declare variables in javascript.
+ * 
+ * 3 Hoisting:
+ * 
+ */
+
+//1 Declaration and assignment
+//declaration
+var color;
+
+//Assignment
+color = "red";
+
+//2 var let and const
+
+//3 hoisting

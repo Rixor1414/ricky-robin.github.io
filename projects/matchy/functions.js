19e77269eb28338e7745edfa2cc847b8c9a38a22
Ricky Robin <softreset13@gmail.com>
2023-08-23 23:43:41 +0000

    describe commit

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -12,26 +12,70 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+//. Implement a function declaration called `search` that:
+ // - Takes a paramater representing an Array of `animals`.
+  // - Takes a paramater representing a String, the name of an animal on which to perform a search.
+  // - Looks through the `animals` Array, and returns the animal's Object if an animal with that name exists.
+  // - Returns `null` if no animal with that name exists
+// 3. Use the search bar at the top of the page to make sure ƒyour function works.
+//console.log(animals)
+function search(array, string){
+    for (var i = 0; i < array.length; i++){
+        if (array[i].name === string){
+            return array[i]
+        }
+    }
+    return null
+}
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+1. //Write a function declaration called `replace` with a signature of `replace(animals, name, replacement) { //... } ` that:
+    //- Takes 3 parameters, an Array of animals, a String representing  the name of an animal on which to perform a search, and an Object that represents the replacement animal.
+    //- If an animal with that name exists within the `animals` Array, replace it's entire Object with the replacement Object.
+   // - Otherwise do nothing.
+  //2. Preview the `index.html` page to test it on the website.
 
-
-
+function replace(animals, name1, replacement) {
+    for (var i = 0; i < animals.length; i++){
+        if (animals[i].name === name1){
+            animals.splice(i, 1, replacement)
+        }
+    }
+}
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+//1. Write a function declaration called `remove` with a signature of `remove(animals, name)` that:
+//- Takes 2 parameters, an Array of animals, and a name of an animal on which to perform a search.
+//- If an animal with that name exists within the `animals` Array, remove it.
+//2. Test that it works on the website.
+function remove(animals, name1){
+    for (var i = 0; i < animals.length; i++){
+        if (animals[i].name === name1){
+            animals.splice(i, 1)
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+// Write a function declaration called `add` with a signature of `add(animals, animal) { //... }` that:
+   // - Takes 2 parameter, an Array of animals, and an Object representing a new animal to be added.
+   // - Checks that the animal Object has a `name` property with a length > 0.
+   // - Checks that the animal Object has a `species` property with a length > 0.
+  //  - Has a **unique** name, meaning no other animals have that name.
+   // - Adds this new Object to the `animals` Array, **only** if all the other conditions pass.
+   // - Make sure it works.
+function add(animals, animal){
+    if (animal.name.length > 0 && animal.species.length > 0){
+        if (search(animals, animal.name) === null){
+            animals.push(animal)
+        }
+        }
+        
+    }
 
 /**
  * You did it! You're all done with Matchy!

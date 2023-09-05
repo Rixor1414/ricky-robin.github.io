367da4f094661503c9f7f67097200df6306320a7
Ricky Robin <softreset13@gmail.com>
2023-09-05 01:56:06 +0000

    describe commit

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -9,7 +9,7 @@ var factorial = function(n) {
   if (n < 0){
     return null
   }
-  if (n === 0 || n === 1) {
+  if (n === 0) {
     return 1
   } else {
     return n * factorial(n - 1)
@@ -37,24 +37,47 @@ var isEven = function(n) {
   if (n === 0) {
     return true;
   } else if (n === 1) {
-    return false;
+    return false
   } else if (n < 0) {
-    return isEven(-n);
+    return isEven(-n)
   } else {
-    return isEven(n - 2);
+    return isEven(n - 2)
   }
-};
+}
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n < 0) {
+    return n + 1 + sumBelow(n + 1);
+  } else {
+    return n - 1 + sumBelow(n - 1);
+  }
 };
 
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
 var range = function(x, y) {
-};
+  if (x === y) {
+    return [];
+  } else if (x < y) {
+    if (x + 1 === y) {
+      return []
+    } else {
+      return [x + 1].concat(range(x + 1, y))
+    }
+  } else {
+    if (x - 1 === y) {
+      return []
+    } else {
+      return [x - 1].concat(range(x - 1, y))
+    }
+  }
+}
+
 
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
